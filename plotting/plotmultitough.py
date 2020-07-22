bb8e1ef91e044a730f4dfb7ef9f543018e34ad3e
Temitope Ajayi <na_me_men@yahoo.com>
2020-07-22 03:29:29 -0500

    modified files

--- plotting/plotmultitough.py
+++ plotting/plotmultitough.py
@@ -9,16 +9,24 @@ from fileparser import tough3, toughreact
 class PlotMultiTough(object):
     def __init__(self, simulatortype, filelocations, filetitles):
         self.filelocations = filelocations
-        os.chdir(self.filelocations)
         self.filetitles = filetitles
         self.simulatortype = simulatortype
         self.modifier = processor.Utilities()
 
     def read_file(self):
+        os.chdir(self.filelocations)
         if self.simulatortype.lower() == "tmvoc" or self.simulatortype.lower() == "tough3":
             fileReader = tough3.Tough3(self.simulatortype, self.filelocations, self.filetitles)
         else:
             fileReader = toughreact.Toughreact(self.simulatortype, self.filelocations, self.filetitles)
+        return fileReader
+
+    def read_file_multi(self, file, filetitle):
+        os.chdir(file)
+        if self.simulatortype.lower() == "tmvoc" or self.simulatortype.lower() == "tough3":
+            fileReader = tough3.Tough3(self.simulatortype, file, filetitle)
+        else:
+            fileReader = toughreact.Toughreact(self.simulatortype, file, filetitle)
         return fileReader
 
     def multi_time_plot(self, param, gridblocknumber, style='horizontal'):
@@ -68,3 +76,47 @@ class PlotMultiTough(object):
             plt.tight_layout()
             plt.show()
             fig.savefig('Multi plot' + ' vs ' + 'time' + '.png', bbox_inches='tight', dpi=600)
+
+    # def retrieve_multi_data(self, param, gridblocknumber):
+    #     dataStorage = {}
+    #     for file in self.filelocations:
+    #         fileNumber = 0
+    #         for parameter in param:
+    #             fileReader = self.read_file_multi(file, self.filetitles[fileNumber])
+    #             filename = parameter + str(fileNumber)
+    #             dataStorage[filename] = fileReader.get_timeseries_data(parameter, gridblocknumber)
+    #             fileNumber = fileNumber + 1
+    #     return dataStorage
+
+    def retrieve_multi_data(self, param, gridblocknumber):
+        dataStorage = {}
+        fileNames = []
+        for parameter in param:
+            fileNumber = 0
+            for file in self.filelocations:
+                fileReader = self.read_file_multi(file, self.filetitles[fileNumber])
+                filename = parameter + str(fileNumber)
+                dataStorage[filename] = fileReader.get_timeseries_data(parameter, gridblocknumber)
+                fileNames.append(filename)
+                fileNumber = fileNumber + 1
+        return fileNames, dataStorage
+
+
+    def multi_param_multi_file_plot(self, param, gridblocknumber, labels, style='horizontal', width=12, height=8):
+        fig = plt.figure(figsize=(width, height))
+        paralengthdouble = len(param) * 2
+        fileReader = self.read_file()
+        time_year = fileReader.convert_times_year()
+        lst, dictionary = self.retrieve_multi_data(param, gridblocknumber)
+        colors = ['r', 'royalblue', 'g', 'k', 'c', 'm', 'y']
+        markers = ["o", "v", "^", "<", ">", "1", "2", "3", "4", "8", "s", "p", "P", "*", "h", "H", "+", "x", "X", "D",
+                   "d", "|", "_"]
+        j = 0
+        kpansa = 0
+        counter = 1
+        for number in range(1, len(param) + 1):
+            axs = plt.subplot(3, 2, counter)
+            k = 0
+            for i in range(kpansa, len(dictionary)):
+                axs.plot(time_year, dictionary[lst[i]], label=labels[k], linewidth=2,
+                         color=colors[k], marker=markers[k])
