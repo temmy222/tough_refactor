27fd3f444398d5c50e566a2b65b4bddd829d9c0d
Temitope Ajayi <na_me_men@yahoo.com>
2020-07-22 00:35:47 -0500

    new modifications

--- plotting/plottough.py
+++ plotting/plottough.py
@@ -13,6 +13,7 @@ import utils.utilities as processor
 import fileparser.fileread as fileDetails
 import fileparser.tough3 as tough3
 import fileparser.toughreact as toughreact
+import pandas as pd
 
 
 class PlotTough(object):
@@ -64,6 +65,7 @@ class PlotTough(object):
         # axs.legend(loc='upper right', borderpad=0.1)
         plt.tight_layout()
         plt.show()
+        fig.savefig(param + 'vs' + 'time' + '.png', bbox_inches='tight', dpi=600)
 
     def plotParamWithParam(self, param1, param2, gridblocknumber):
 
@@ -72,7 +74,7 @@ class PlotTough(object):
         result_array_y = fileReader.get_timeseries_data(param2, gridblocknumber)
         fig, axs = plt.subplots(1, 1)
         axs.plot(result_array_x, result_array_y, marker='^')
-        axs.set_xlabel('Time (year)')
+        axs.set_xlabel(self.modifier.param_label_full(param1.upper()))
         axs.set_ylabel(self.modifier.param_label_full(param2.upper()))
         ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
         CONFIG_PATH = os.path.join(ROOT_DIR, 'mystyle.mplstyle')
@@ -80,6 +82,7 @@ class PlotTough(object):
         plt.style.use(CONFIG_PATH)
         plt.tight_layout()
         plt.show()
+        fig.savefig(param2 + ' vs ' + param1 + '.png', bbox_inches='tight', dpi=600)
 
     def plot2D_one(self, direction1, direction2, param, timer):
         fileReader = self.read_file()
@@ -108,53 +111,56 @@ class PlotTough(object):
         plt.tick_params(axis='y', labelsize=12)
         plt.tight_layout()
         plt.show()
+        fig.savefig('2D plain' + str(timer) + param + '.png', bbox_inches='tight', dpi=600)
 
-    def plot2D_withgrid(self, direction1, direction2, param, timer):
+    def plot2D_withgrid(self, direction1, direction2,  param, timer):
         fileReader = self.read_file()
         fig, ax = plt.subplots(1, 1)
         X = fileReader.get_coord_data(direction1, timer)
         Z = fileReader.get_coord_data(direction2, timer)
-        num_X = self.modifier.get_number_of_grids(X)
-        num_Z = self.modifier.get_number_of_grids(Z)
-        print(num_X, num_Z)
-        orig_data = fileReader.get_element_data(timer, param)
+        # param = tre.element[self.parameters[paramNum]]
+        df = pd.DataFrame(index=range(len(X)))
+        df['X'] = X
+        df['Z'] = Z
+        Zvalues, Ztotal = self.modifier.getgridnumber(df, 'Z')
+        Xvalues, Xtotal = self.modifier.getgridnumber(df, 'X')
         xi, yi = np.meshgrid(X, Z)
-        data = np.asarray(fileReader.get_element_data(timer, param))
-        # data = data.reshape(num_Z,num_X)
+        orig_data = fileReader.get_element_data(timer, param)
+        data = np.asarray(orig_data)
+        data = data.reshape(Ztotal, Xtotal)
         data1 = griddata((X, Z), orig_data, (xi, yi), method='nearest')
         extent = [min(X), max(X), min(Z), max(Z)]
-        # cs2 = plt.pcolor(xi,yi,data1,edgecolors='k',cmap='coolwarm', linewidths=1,vmin=min(orig_data), vmax=max(orig_data))
-        # ax.imshow(data,extent=extent)
-        # ax.grid(color='k', linestyle='-', linewidth=2)
-        # ax.set_frame_on(False)
-        # cbar = fig.colorbar(cs2,ax=ax,pad=0.01)
-        # cbar.ax.set_ylabel(self.param_label_full(param.upper()),fontsize=12)
-        # plt.xlabel('Horizontal Distance(m)',fontsize=12)
-        # plt.ylabel('Vertical Depth (m)',fontsize=12)
-        # plt.tick_params(axis='x', labelsize=12)
-        # plt.tick_params(axis='y', labelsize=12)
-        # plt.tight_layout()
-        # plt.figure()
-        cs2 = plt.imshow(np.reshape(data, newshape=(num_Z, num_X)), cmap='coolwarm', interpolation='none')
+        cs2 = plt.imshow(np.reshape(data, newshape=(Ztotal, Xtotal)), cmap='coolwarm', interpolation='none')
         ax = plt.gca()
         # Major ticks
-        ax.set_xticks(np.arange(0, num_X, 6))
-        ax.set_yticks(np.arange(0, num_Z, 1))
+        x_tick = np.arange(0, Xtotal, 4)
+        z_tick = np.arange(0, Ztotal, 1)
+        Z_array = np.asarray(Z)
+        Z_array = np.abs(Z_array)
+        ax.set_xticks(x_tick)
+        num_tick_x = len(x_tick)
+        num_tick_z = len(z_tick)
+        ax.set_yticks(z_tick)
         # Labels for major ticks
-        ax.set_xticklabels(np.arange(1, round(max(X))+1, 6), fontsize=12)
-        ax.set_yticklabels(np.arange(abs(max(Z))+2, abs(min(Z))+3, 5), fontsize=12)
+        tick_x = max(X)-min(X)
+        tick_z = max(Z)-min(Z)
+        ax.set_xticklabels(np.round(self.modifier.crange(min(X), max(X), tick_x/(num_tick_x-1)), 2), fontsize=8)
+        ax.set_yticklabels(np.round(self.modifier.crange(min(Z_array), max(Z_array), tick_z/(num_tick_z-1)), 2), fontsize=8)
         # Minor ticks
-        ax.set_xticks(np.arange(-.5, num_X, 1), minor=True);
-        ax.set_yticks(np.arange(-.5,num_Z, 1), minor=True);
+        ax.set_xticks(np.arange(-.5, Xtotal, 1), minor=True);
+        ax.set_yticks(np.arange(-.5, Ztotal, 1), minor=True);
         # Gridlines based on minor ticks
         ax.grid(which='minor', color='k', linestyle='-', linewidth=1)
-        cbar = fig.colorbar(cs2, ax=ax, pad=0.01)
-        cbar.ax.set_ylabel(self.modifier.param_label_full(param.upper()), fontsize=12)
-        plt.xlabel('Horizontal Distance(m)', fontsize=12)
-        plt.ylabel('Vertical Depth (m)', fontsize=12)
+        cbar = fig.colorbar(cs2, ax=ax, pad=0.2, orientation="horizontal")
+        # cbar.ax.set_ylabel(param,fontsize=8)
+        cbar.ax.set_title(param, fontsize=8)
+        cbar.ax.tick_params(labelsize=8)
+        cbar.ax.locator_params(nbins=7)
+        plt.xlabel('Horizontal Distance(m)', fontsize=8)
+        plt.ylabel('Vertical Depth (m)', fontsize=8)
         plt.tight_layout()
         plt.show()
-        print(abs(max(Z)), abs(min(Z)))
+        fig.savefig('Grid' + str(timer) + param +'.png', bbox_inches='tight', dpi=600)
 
     def plotstyle(self):
         axs.spines['bottom'].set_linewidth(1.5)
