e8b26db219a54ff3ceda586aa6f9589f7335a10e
temmy222 <na_me_men@yahoo.com>
2021-02-04 11:41:59 -0600

    plotting

--- RunScripts/paper_2.py
+++ RunScripts/paper_2.py
@@ -2,11 +2,11 @@ from fileparser.fileread import FileReadSingle
 
 
 loca1 = r"C:\Users\tajayi3\OneDrive - Louisiana State University\Second numerical paper\flow through brine only"
-loca2 = r"C:\Users\tajayi3\OneDrive - Louisiana State University\Second numerical paper\Python Run\Base-Python-Paper-2"
+loca2 = r"C:\Users\tajayi3\OneDrive - Louisiana State University\Second numerical paper\Python Run\Shale-Sand-Shale-Limestone"
 
 filetype_toughreact = 'kdd_conc.tec'
 
 
 testcodetoughreact = FileReadSingle("toughreact", loca2, filetype_toughreact)
 
-testcodetoughreact.plot2D('x', 'z', 'pH', 2.592e+2, 'grid')
+testcodetoughreact.plot2D('x', 'z', 'pH', 2.592e+10, 'grid')
