003b276c8f2617f358f444f5f42571916d738c63
temmy222 <na_me_men@yahoo.com>
2021-11-09 17:14:53 -0600

    new_material

--- RunScripts/new_material.py
+++ RunScripts/new_material.py
@@ -1,9 +1,19 @@
 from fileparser.fileread import FileReadMultiple
 
-file1 = r"D:\Working Folder - Ajayi\my TOUGHREACT$TOUGH Simulations\Moving Forward\Paper Flow\For paper\GOM Sandstone Cement flow with Batch\Increased depth\Gulf of Mexico Sandstone Cement Flow - Ca injected brine Offshore"
-file2 = r"D:\Working Folder - Ajayi\One Drive - LSU\OneDrive - Louisiana State University\Second numerical paper\Python Run\new material\Gulf of Mexico Cement Flow - Ca injected sand equil brine Offshore - longer time"
+file2 = r"D:\Working Folder - Ajayi\my TOUGHREACT$TOUGH Simulations\Moving Forward\Paper Flow\For paper\GOM Sandstone Cement flow with Batch\Increased depth\Gulf of Mexico Sandstone Cement Flow - Ca injected brine Offshore"
+file1 = r"D:\Working Folder - Ajayi\One Drive - LSU\OneDrive - Louisiana State University\Second numerical paper\Python Run\new material\Gulf of Mexico Cement Flow - Ca injected sand equil brine Offshore - longer time"
+file3 = r"D:\Working Folder - Ajayi\my TOUGHREACT$TOUGH Simulations\Moving Forward\Paper Flow\For paper\GOM Sandstone Cement flow with Batch\Increased depth\Gulf of Mexico Sandstone Cement Flow - Ca injected brine Onshore"
+file4 = r"D:\Working Folder - Ajayi\One Drive - LSU\OneDrive - Louisiana State University\Second numerical paper\Python Run\new material\Gulf of Mexico Cement Flow - Ca injected sand equil brine Onshore - longer time"
+file5 = r"D:\Working Folder - Ajayi\my TOUGHREACT$TOUGH Simulations\Moving Forward\Paper Flow\For paper\GOM Sandstone Cement flow with Batch\Increased depth\Gulf of Mexico Sandstone Cement Flow - Na acetate brine"
+file6 = r"D:\Working Folder - Ajayi\One Drive - LSU\OneDrive - Louisiana State University\Second numerical paper\Python Run\new material\Gulf of Mexico Cement Flow - Na acetate sand equil injected Onshore - longer time"
+file7 = r"D:\Working Folder - Ajayi\my TOUGHREACT$TOUGH Simulations\Moving Forward\Paper Flow\For paper\GOM Sandstone Cement flow with Batch\Increased depth\Gulf of Mexico Sandstone Cement Flow - NaCl brine"
+file8 = r"D:\Working Folder - Ajayi\One Drive - LSU\OneDrive - Louisiana State University\Second numerical paper\Python Run\new material\Gulf of Mexico Cement Flow - NaCl sand equil brine injected Onshore - longer"
 
 all_toughreact_files = [file1, file2]
+all_toughreact_files2 = [file4, file3]
+all_toughreact_files3 = [file6, file5]
+all_toughreact_files4 = [file8, file7]
+
 param_min = ['calcite', 'portlandite', 'Porosity', 'ettringite']
 
 legend = ['Status quo', 'New Material']
@@ -12,6 +22,14 @@ filetype_toughreact_min = 'kdd_min.tec'
 all_toughreact_filetypes = [filetype_toughreact_min, filetype_toughreact_min, filetype_toughreact_min, filetype_toughreact_min]
 
 testcodemultitoughreact = FileReadMultiple("toughreact", all_toughreact_files, all_toughreact_filetypes, param_min)
-testcodemultitoughreact.plotTime(83, legend, plot_kind='file')
+testcodemultitoughreact.plotTime(0, legend, plot_kind='file')
+testcodemultitoughreact = FileReadMultiple("toughreact", all_toughreact_files2, all_toughreact_filetypes, param_min)
+testcodemultitoughreact.plotTime(0, legend, plot_kind='file')
+testcodemultitoughreact = FileReadMultiple("toughreact", all_toughreact_files3, all_toughreact_filetypes, param_min)
+testcodemultitoughreact.plotTime(0, legend, plot_kind='file')
+testcodemultitoughreact = FileReadMultiple("toughreact", all_toughreact_files4, all_toughreact_filetypes, param_min)
+testcodemultitoughreact.plotTime(0, legend, plot_kind='file')
+
+
 
 print('correct')
